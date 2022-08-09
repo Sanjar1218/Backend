7b36f9f53e76940e3f8f690c1be9dd371af174a2
sanjarbek <sanjarsaidov173@gamil.com>
2022-08-09 10:18:41 +0500

    status_code

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ def get_male_data(file_name = 'data.json', n=1):
     lst = []
     dct = {} 
     while i<n:
-        try:
-            r = requests.get('https://randomuser.me/api').json()
-            # print(r.json())
-            result = r['results'][0]
-            
+
+        r = requests.get('https://randomuser.me/api')
+        # print(r.json())
+        if r.status_code==200:
+            data = r.json()
+            result = data['results'][0]
             dct ={} 
             if result['gender'] == 'male':
                 i+=1
@@ -27,11 +28,8 @@ def get_male_data(file_name = 'data.json', n=1):
                 dct['city'] = result['location']['country']
                 dct['age'] = result['dob']['age']
                 lst.append(dct)
-        except:
-            print('e')
-            continue
     dct1 = {'results':lst}
     json.dump(dct1, f, ensure_ascii=False, indent=2)
     f.close()
 
-get_male_data('data1', 3)
+get_male_data('data1', 100)
